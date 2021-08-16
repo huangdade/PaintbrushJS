4e80f909458867aeeb97d9bc28a35f9487d708e2
bfbrmt <bfbrmt@163.com>
2021-08-16 15:17:49 +0800

    优化

--- demo/script/playground.js
+++ demo/script/playground.js
@@ -1,357 +1,163 @@
-// --------------------------------------------------
-//
-// playground.js, v0.2
-// supporting script for PaintbrushJS playground demo
-//
-// Developed by Dave Shea, http://www.mezzoblue.com/
-//
-// This project lives on GitHub:
-//    http://github.com/mezzoblue/PaintbrushJS
-//
-// Except where otherwise noted, PaintbrushJS is licensed under the MIT License:
-//    http://www.opensource.org/licenses/mit-license.php
-//
-// --------------------------------------------------
 
+let originImage = document.getElementById("original-image");
+let resultImage = document.getElementById("result-image");
 
-
-// filter master control object
-var filterControl = {
-	"filters"	:	[
-
+// 滤镜列表：用于创建下拉列表，以及建立与controls的对应关系
+const filterControl = {
+	"filters":	[
 		{
 			"name"			: "blur",
-			"label"			: "Gaussian Blur",
+			"label"			: "高斯模糊",
 			"filterClass"	: "filter-blur",
 			"controlId"		: "controls-blur"
 		},
-
-
 		{
 			"name"			: "edges",
-			"label"			: "Detect Edges",
+			"label"			: "检测边缘",
 			"filterClass"	: "filter-edges",
 			"controlId"		: "controls-edges"
 		},
-
 		{
 			"name"			: "emboss",
-			"label"			: "Emboss",
+			"label"			: "浮凸",
 			"filterClass"	: "filter-emboss",
 			"controlId"		: "controls-emboss"
 		},
-
 		{
 			"name"			: "greyscale",
-			"label"			: "Greyscale",
+			"label"			: "灰度化",
 			"filterClass"	: "filter-greyscale",
 			"controlId"		: "controls-greyscale"
 		},
-
-
 		{
 			"name"			: "mosaic",
-			"label"			: "Mosaic",
+			"label"			: "马赛克",
 			"filterClass"	: "filter-mosaic",
 			"controlId"		: "controls-mosaic"
 		},
-
 		{
 			"name"			: "noise",
-			"label"			: "Noise",
+			"label"			: "噪点",
 			"filterClass"	: "filter-noise",
 			"controlId"		: "controls-noise"
 		},
-
 		{
 			"name"			: "posterize",
-			"label"			: "Posterize",
+			"label"			: "色调分离",
 			"filterClass"	: "filter-posterize",
 			"controlId"		: "controls-posterize"
 		},
-		
 		{
 			"name"			: "sepia",
-			"label"			: "Sepia Tone",
+			"label"			: "老照片特效",
 			"filterClass"	: "filter-sepia",
 			"controlId"		: "controls-sepia"
 		},
-
-		{
+    	{
 			"name"			: "sharpen",
-			"label"			: "Sharpen",
+			"label"			: "锐化",
 			"filterClass"	: "filter-sharpen",
 			"controlId"		: "controls-sharpen"
 		},
-
 		{
 			"name"			: "tint",
-			"label"			: "Colour Tint",
+			"label"			: "色调",
 			"filterClass"	: "filter-tint",
 			"controlId"		: "controls-tint"
 		}
 	]
-}
+};
 
-
-// filter selection events
-addLoadEvent(function() {
-
-	// get the main image
-	var img = getFilterTarget();
-	// get the filter chooser
-	var filterSelector = getFilterSelector();
-
-	// populate the select list with defined filters
-	for (var i = 0; i < filterControl.filters.length; i++) {
-
-		// create the new option element
-		newOption = document.createElement("option");
-		newOption.text = filterControl.filters[i].label;
-		newOption.value = filterControl.filters[i].name;
-
-		// add it to the list
-		filterSelector.appendChild(newOption);
-	}
-
-	// add an event handler to swap control panels
-	filterSelector.onchange = function() {
-		removeClasses(img);
-		flushDataAttributes(img);
-		destroyStash(img, true);
-		// adjust visible control panel
-		displayControls();
-		updateFilters(img);
-	}
-	
-	// initialize the control panel
-	displayControls();
-
-});
-
-
-
-
-// thumbnail / photo swapping
-addLoadEvent(function() {
-
-	// get the main image
-	var img = getFilterTarget();
-	// get every element with the specified thumb class
-	var thumbParents = getElementsByClassName("thumb");
-	
-	// walk through each thumbnail and attach an event handler
-	for (var i = 0; i < thumbParents.length; i++) {
-		var thumbs = thumbParents[i].getElementsByTagName("img");
-		for (var j = 0; j < thumbs.length; j++) {
-		
-			// swap the main image with the thumbnail source on click
-			thumbs[j].onclick = function() {
-			
-				// clear classes on parent li elements
-				var parentLi = this.parentNode;
-				var parentUl = parentLi.parentNode;
-				var lis = parentUl.getElementsByTagName("li");
-				for (var k = 0; k < lis.length; k++) {
-					removeClass(lis[k], "current");
-				}
-
-				// add a "current" class to highlight current thumbnail
-				addClass(parentLi, "current");
-				
-			
-				// clean up old image
-				removeClasses(img);
-				destroyStash(img);
-				img.src = this.getAttribute("src");
-				
-				// re-apply filters
-				displayControls();
-				updateFilters(img);
-			}
-			
-		}
-	}
-
-});
-
-		
-// add events to update the image upon various interactions
-addLoadEvent(function() {
-
-	// get our two main interaction objects, the form and the image
-	var thisForm = getInteractionForm();
-	var interaction = getInteractionTarget();
-	var img = getFilterTarget();
-
-	// initial filter application
-	updateFilters(img);
-
-	// when the form is submitted...
-	thisForm.onsubmit = function() {
-		updateFilters(img);
-		return false;
-	};
-
-	// also add a hook for range sliders
-	inputs = interaction.getElementsByTagName("input");
-
-	for (var i = 0; i < inputs.length; i++) {
-		if (inputs[i].type.toLowerCase() == "range") {
-			// kills performance if we're calling it onchange.
-			// but inaccessible / irrelevant to mobile if we do it onmouseup.
-			// damned if we do... I'm favouring speed here, would love input.
-			inputs[i].onmouseup = function() {		
-				updateFilters(img);
-			}
-		}
-		// fallback for radio buttons, colour input
-		// (and browsers that don't do range yet, hint hint Firefox)
-		if ((inputs[i].type.toLowerCase() == "text") || (inputs[i].type.toLowerCase() == "radio")) {
-			inputs[i].onchange = function() {
-				updateFilters(img);
-			}
-		}
-	}
-
-
-});
-
-
-
-
-
-// refresh filters after various actions have been performed
-function updateFilters(img) {
-
-	// first, does the reference object have an original image in the DOM?
-	var classList = (img.className.toLowerCase()).split(' ');
-	for (var i = 0; i < classList.length; i++) {
-	
-		// quick reference
-		var currentClass = classList[i];
-
-		// okay, we're good, there's an original
-		if (currentClass.substr(0, 7) == "pb-ref-") {
-
-			// clear reference object's data-pb-* attributes
-			flushDataAttributes(img);
-
-			// get the filter chooser
-			var filterSelector = getFilterSelector();
-
-			// go fetch the original and update the reference object source
-			var original = document.getElementById("pb-original-" + currentClass.substr(7, currentClass.length - 7));
-			placeReferenceImage(img, original.src, img);
-			img.style.visibility = "hidden";
-
-			// find the currently-selected filter
-			var options = filterSelector.getElementsByTagName("option");
-			var obj = getCurrentControl(options);
-			if (obj) {
-
-				// get all inputs in the form
-				currentForm = document.getElementById(obj.controlId);
-				inputs = currentForm.getElementsByTagName("input");
-	
-				// loop through the inputs, grabbing the names and values and 
-				// setting them as attributes on the reference object
-				for (var j = 0; j < inputs.length; j++) {
-					if ((inputs[j].type == "range") || (inputs[j].type == "text")) addAttribute(img, inputs[j].name, inputs[j].value);
-					if ((inputs[j].type == "radio")) {
-						if (inputs[j].checked) {
-							addAttribute(img, inputs[j].name, inputs[j].value);
-						}
-					}
-				}
-			}
-		}
-	}
-
-	// finally, apply the filters
-	processFilters();
-
-	img.style.visibility = "visible";
-}
-
-
-
-
-
-// update to current filter controls when switching filters
+// 切换控件显示
 function displayControls() {
-	// get the main image
-	var img = getFilterTarget();
-	// get the filter chooser
-	var filterSelector = getFilterSelector();
-
-	// set up the control panels
-	hideControls();
-	showCurrentControl(filterSelector);
-	flushDataAttributes(img);
-	removeClasses(img);
-
-	// find the currently-selected filter
-	var options = filterSelector.getElementsByTagName("option");
-	var obj = getCurrentControl(options);
-	if (obj) {
-		// if we've got a match, display this current filter's control panel
-		addClass(img, obj.filterClass);
-	}
-
-	// kinda weird that I need to apply this inside and outside the filter. ideally it'd only happen here.
-	// Haven't quite tracked down why the 2x application is necessary yet.
-	updateFilters(img);
+    // 查询当前选中项
+    let selectedFilter = filterSelector.options[filterSelector.selectedIndex].value;
+	let obj = filterControl.filters.find(item => item.name === selectedFilter);
+    let current = obj.controlId;
+    // 全部控件
+	let controls = document.getElementsByClassName("controls");
+	for (let i = 0; i < controls.length; i++) {
+        let id = controls[i].id;
+        if (id === current) {
+            controls[i].style.display = "block";
+        } else {
+            controls[i].style.display = "none";
+        }
+    }
 }
 
+// 滤镜下拉列表
+let filterSelector = document.getElementById("filter-selector");
+for (let i = 0; i < filterControl.filters.length; i++) {
+    let newOption = document.createElement("option");
+    newOption.text = filterControl.filters[i].label;
+    newOption.value = filterControl.filters[i].name;
 
-// hide all control panels
-function hideControls() {
-	var controls = getElementsByClassName("controls");
-	for (var i = 0; i < controls.length; i++) {
-		controls[i].style.display = "none";
-	}
+    filterSelector.appendChild(newOption);
 }
 
-// show currently-selected control panel
-function showCurrentControl(filterSelector) {
+displayControls();
+originImage.onload = updateFilters;
+originImage.src = "images/500/stones.jpg";
 
-	// find the currently-selected filter
-	var options = filterSelector.getElementsByTagName("option");
-	var obj = getCurrentControl(options);
-	if (obj) {
-		// if we've got a match, display this current filter's control panel
-		var current = document.getElementById(obj.controlId);
-		current.style.display = "block";
+// 给所有的控件添加监听事件，触发图片处理
+for (let i = 0; i < filterControl.filters.length; i++) {
+    let id = filterControl.filters[i].controlId;
+    let control = document.getElementById(id);
+    let inputs = control.getElementsByTagName("input");
+	for (let j = 0; j < inputs.length; j++) {
+        inputs[j].addEventListener("change", updateFilters);
 	}
 }
-
-// helper functions to grab the needed DOM elements
-function getFilterSelector() {
-	return document.getElementById("filter-selector");
-}
-function getFilterTarget() {
-	return document.getElementById("filter-target");
-}
-function getInteractionForm() {
-	return document.getElementById("interaction-form");
-}
-function getInteractionTarget() {
-	return document.getElementById("interaction");
+filterSelector.onchange = function () {
+    displayControls();
+    updateFilters();
 }
 
-// helper function to match the currently-selected option in the select list to a filter control
-function getCurrentControl(options) {
-	// first, loop through the list of select options
-	for (var i = 0; i < options.length; i++) {
-		// when we find the currently-selected one, loop through the filter control object
-		if (options[i].selected) {
-			for (var j = 0; j < filterControl.filters.length; j++) {
-				// if we can match the selected option to a filter, return the filter
-				if (filterControl.filters[j].name == options[i].value) {
-					return(filterControl.filters[j]);
-				}
-			}
+// 对图片应用滤镜，并将结果展示出来
+const processCanvas = document.createElement("canvas");
+const processContext = processCanvas.getContext("2d");
+function updateFilters() {
+    // 提取当前滤镜类别
+    let selectedFilter = filterSelector.options[filterSelector.selectedIndex].value;
+	let obj = filterControl.filters.find(item => item.name === selectedFilter);
+	processFilters(originImage, obj.filterClass, getParamters(), processCanvas, processContext);
+    resultImage.src = processCanvas.toDataURL("image/png");
+}
+
+// 从控件中提取参数
+function getParamters() {
+    // 当前滤镜类别
+    let selectedFilter = filterSelector.options[filterSelector.selectedIndex].value;
+    // 获取对应的control
+	let obj = filterControl.filters.find(item => item.name === selectedFilter);
+    let currControl = document.getElementById(obj.controlId);
+    // 提取该control中的input
+	let inputs = currControl.getElementsByTagName("input");
+    // 依次从input中提取参数
+	let params = {};
+	for (var i = 0; i < inputs.length; i++) {
+        let name = inputs[i].name; // input的默认名称
+        let name2 = name.substr(8); // 去掉前缀
+        // 获取连字符下标
+        let hyphenIndex = name2.indexOf('-');
+        // 改成驼峰式
+        name2 = name2.substr(0, hyphenIndex) + name2.charAt(hyphenIndex + 1).toUpperCase() + name2.substr(hyphenIndex + 2);
+        let type = inputs[i].type.toLowerCase();
+        // 提取值：range转成数值
+		if (type === "range") {
+            params[name2] = Number(inputs[i].value);
+		} else if ((type === "text") || (type === "radio")) {
+			params[name2] = inputs[i].value;
 		}
 	}
+
+    if (obj.filterClass === "filter-posterize") {
+        // 这两个参数换算一下，得到一个计算中间值，以便加快计算速度
+        params['posterizeAreas'] = 256 / params.posterizeAmount;
+        params['posterizeValues'] = 255 / (params.posterizeAmount - 1);
+    }
+
+    return params;
 }
+
